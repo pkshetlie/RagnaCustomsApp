52b9c6387a9df67b5f1c393224b37a05e75487f3
Davy <d.braillon@id2i.net>
2021-06-01 19:11:07 +0200

    Fix error while saving api key

--- src/RagnaCustoms.App/Services/Configuration.cs
+++ src/RagnaCustoms.App/Services/Configuration.cs
@@ -17,10 +17,14 @@ namespace RagnaCustoms.Services
             set => Set(nameof(SendScoreAutomatically), value ? bool.TrueString : bool.FalseString);
         }
 
-        private string Get(string key) => ConfigurationManager.AppSettings[key];
+        private string Get(string key)
+        {
+            var config = ConfigurationManager.OpenExeConfiguration(Application.ExecutablePath);
+            return config.AppSettings.Settings[key].Value;
+        }
         private void Set(string key, string value)
         {
-            var config = ConfigurationManager.OpenExeConfiguration(Application.ExecutablePath.Replace(".exe", ".dll"));
+            var config = ConfigurationManager.OpenExeConfiguration(Application.ExecutablePath);
             config.AppSettings.Settings[key].Value = value;
             config.Save(ConfigurationSaveMode.Modified);
         }
