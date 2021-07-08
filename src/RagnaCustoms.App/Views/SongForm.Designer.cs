12811aa5dee5b4858d6bbab404a06195e2323296
pkshetlie <pierrick.pobelle@gmail.com>
2021-07-08 20:53:32 +0200

    add easy access to logfile

--- src/RagnaCustoms.App/Views/SongForm.Designer.cs
+++ src/RagnaCustoms.App/Views/SongForm.Designer.cs
@@ -33,6 +33,12 @@ namespace RagnaCustoms.Views
             this.SearchTextBox = new System.Windows.Forms.TextBox();
             this.SearchButton = new System.Windows.Forms.Button();
             this.SearchResultGridView = new System.Windows.Forms.DataGridView();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SongName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SongDifficulties = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SongAuthor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SongMapper = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SongDownload = new System.Windows.Forms.DataGridViewButtonColumn();
             this.Menu = new System.Windows.Forms.MenuStrip();
             this.FileMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.ExitMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -40,29 +46,23 @@ namespace RagnaCustoms.Views
             this.SendScoreAutomaticallyMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.ApiKeyMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.HelpMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.AboutMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.SongName = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.SongDifficulties = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.SongAuthor = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.SongMapper = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.SongDownload = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.logFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.SearchResultGridView)).BeginInit();
             this.Menu.SuspendLayout();
             this.SuspendLayout();
             // 
             // SearchTextBox
             // 
-            this.SearchTextBox.Location = new System.Drawing.Point(12, 27);
+            this.SearchTextBox.Location = new System.Drawing.Point(10, 23);
             this.SearchTextBox.Name = "SearchTextBox";
-            this.SearchTextBox.Size = new System.Drawing.Size(519, 23);
+            this.SearchTextBox.Size = new System.Drawing.Size(445, 20);
             this.SearchTextBox.TabIndex = 1;
             // 
             // SearchButton
             // 
-            this.SearchButton.Location = new System.Drawing.Point(537, 27);
+            this.SearchButton.Location = new System.Drawing.Point(460, 23);
             this.SearchButton.Name = "SearchButton";
-            this.SearchButton.Size = new System.Drawing.Size(75, 23);
+            this.SearchButton.Size = new System.Drawing.Size(64, 20);
             this.SearchButton.TabIndex = 2;
             this.SearchButton.Text = "Search";
             this.SearchButton.UseVisualStyleBackColor = true;
@@ -82,81 +82,14 @@ namespace RagnaCustoms.Views
             this.SongAuthor,
             this.SongMapper,
             this.SongDownload});
-            this.SearchResultGridView.Location = new System.Drawing.Point(12, 56);
+            this.SearchResultGridView.Location = new System.Drawing.Point(10, 49);
             this.SearchResultGridView.Name = "SearchResultGridView";
             this.SearchResultGridView.ReadOnly = true;
             this.SearchResultGridView.RowHeadersVisible = false;
             this.SearchResultGridView.RowTemplate.Height = 25;
-            this.SearchResultGridView.Size = new System.Drawing.Size(600, 373);
+            this.SearchResultGridView.Size = new System.Drawing.Size(514, 323);
             this.SearchResultGridView.TabIndex = 3;
             this.SearchResultGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.SearchResultGridView_CellContentClick);
-            // 
-            // Menu
-            // 
-            this.Menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.FileMenuItem,
-            this.ToolsMenuItem,
-            this.HelpMenuItem});
-            this.Menu.Location = new System.Drawing.Point(0, 0);
-            this.Menu.Name = "Menu";
-            this.Menu.Size = new System.Drawing.Size(624, 24);
-            this.Menu.TabIndex = 0;
-            this.Menu.Text = "menuStrip1";
-            // 
-            // FileMenuItem
-            // 
-            this.FileMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.ExitMenuItem});
-            this.FileMenuItem.Name = "FileMenuItem";
-            this.FileMenuItem.Size = new System.Drawing.Size(37, 20);
-            this.FileMenuItem.Text = "&File";
-            // 
-            // ExitMenuItem
-            // 
-            this.ExitMenuItem.Name = "ExitMenuItem";
-            this.ExitMenuItem.Size = new System.Drawing.Size(93, 22);
-            this.ExitMenuItem.Text = "E&xit";
-            this.ExitMenuItem.Click += new System.EventHandler(this.ExitMenuItem_Click);
-            // 
-            // ToolsMenuItem
-            // 
-            this.ToolsMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.SendScoreAutomaticallyMenuItem,
-            this.ApiKeyMenuItem});
-            this.ToolsMenuItem.Name = "ToolsMenuItem";
-            this.ToolsMenuItem.Size = new System.Drawing.Size(46, 20);
-            this.ToolsMenuItem.Text = "&Tools";
-            // 
-            // SendScoreAutomaticallyMenuItem
-            // 
-            this.SendScoreAutomaticallyMenuItem.Checked = true;
-            this.SendScoreAutomaticallyMenuItem.CheckOnClick = true;
-            this.SendScoreAutomaticallyMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.SendScoreAutomaticallyMenuItem.Name = "SendScoreAutomaticallyMenuItem";
-            this.SendScoreAutomaticallyMenuItem.Size = new System.Drawing.Size(206, 22);
-            this.SendScoreAutomaticallyMenuItem.Text = "&Send score automatically";
-            this.SendScoreAutomaticallyMenuItem.CheckedChanged += new System.EventHandler(this.SendScoreMenuItem_CheckedChanged);
-            // 
-            // ApiKeyMenuItem
-            // 
-            this.ApiKeyMenuItem.Name = "ApiKeyMenuItem";
-            this.ApiKeyMenuItem.Size = new System.Drawing.Size(206, 22);
-            this.ApiKeyMenuItem.Text = "&Configure API key...";
-            this.ApiKeyMenuItem.Click += new System.EventHandler(this.ApiKeyMenuItem_Click);
-            // 
-            // HelpMenuItem
-            // 
-            this.HelpMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.AboutMenuItem});
-            this.HelpMenuItem.Name = "HelpMenuItem";
-            this.HelpMenuItem.Size = new System.Drawing.Size(44, 20);
-            this.HelpMenuItem.Text = "&Help";
-            // 
-            // AboutMenuItem
-            // 
-            this.AboutMenuItem.Name = "AboutMenuItem";
-            this.AboutMenuItem.Size = new System.Drawing.Size(116, 22);
-            this.AboutMenuItem.Text = "&About...";
             // 
             // Id
             // 
@@ -210,12 +143,81 @@ namespace RagnaCustoms.Views
             this.SongDownload.UseColumnTextForButtonValue = true;
             this.SongDownload.Width = 30;
             // 
+            // Menu
+            // 
+            this.Menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.FileMenuItem,
+            this.ToolsMenuItem,
+            this.HelpMenuItem});
+            this.Menu.Location = new System.Drawing.Point(0, 0);
+            this.Menu.Name = "Menu";
+            this.Menu.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
+            this.Menu.Size = new System.Drawing.Size(535, 24);
+            this.Menu.TabIndex = 0;
+            this.Menu.Text = "menuStrip1";
+            // 
+            // FileMenuItem
+            // 
+            this.FileMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ExitMenuItem});
+            this.FileMenuItem.Name = "FileMenuItem";
+            this.FileMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.FileMenuItem.Text = "&File";
+            // 
+            // ExitMenuItem
+            // 
+            this.ExitMenuItem.Name = "ExitMenuItem";
+            this.ExitMenuItem.Size = new System.Drawing.Size(93, 22);
+            this.ExitMenuItem.Text = "E&xit";
+            this.ExitMenuItem.Click += new System.EventHandler(this.ExitMenuItem_Click);
+            // 
+            // ToolsMenuItem
+            // 
+            this.ToolsMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.SendScoreAutomaticallyMenuItem,
+            this.ApiKeyMenuItem});
+            this.ToolsMenuItem.Name = "ToolsMenuItem";
+            this.ToolsMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.ToolsMenuItem.Text = "&Tools";
+            // 
+            // SendScoreAutomaticallyMenuItem
+            // 
+            this.SendScoreAutomaticallyMenuItem.Checked = true;
+            this.SendScoreAutomaticallyMenuItem.CheckOnClick = true;
+            this.SendScoreAutomaticallyMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.SendScoreAutomaticallyMenuItem.Name = "SendScoreAutomaticallyMenuItem";
+            this.SendScoreAutomaticallyMenuItem.Size = new System.Drawing.Size(206, 22);
+            this.SendScoreAutomaticallyMenuItem.Text = "&Send score automatically";
+            this.SendScoreAutomaticallyMenuItem.CheckedChanged += new System.EventHandler(this.SendScoreMenuItem_CheckedChanged);
+            // 
+            // ApiKeyMenuItem
+            // 
+            this.ApiKeyMenuItem.Name = "ApiKeyMenuItem";
+            this.ApiKeyMenuItem.Size = new System.Drawing.Size(206, 22);
+            this.ApiKeyMenuItem.Text = "&Configure API key...";
+            this.ApiKeyMenuItem.Click += new System.EventHandler(this.ApiKeyMenuItem_Click);
+            // 
+            // HelpMenuItem
+            // 
+            this.HelpMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.logFileToolStripMenuItem});
+            this.HelpMenuItem.Name = "HelpMenuItem";
+            this.HelpMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.HelpMenuItem.Text = "&Help";
+            // 
+            // logFileToolStripMenuItem
+            // 
+            this.logFileToolStripMenuItem.Name = "logFileToolStripMenuItem";
+            this.logFileToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.logFileToolStripMenuItem.Text = "Log file";
+            this.logFileToolStripMenuItem.Click += new System.EventHandler(this.logFileToolStripMenuItem_Click);
+            // 
             // SongForm
             // 
             this.AcceptButton = this.SearchButton;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(624, 441);
+            this.ClientSize = new System.Drawing.Size(535, 382);
             this.Controls.Add(this.SearchResultGridView);
             this.Controls.Add(this.SearchButton);
             this.Controls.Add(this.SearchTextBox);
@@ -245,7 +247,6 @@ namespace RagnaCustoms.Views
         private System.Windows.Forms.ToolStripMenuItem ToolsMenuItem;
         private System.Windows.Forms.ToolStripMenuItem SendScoreAutomaticallyMenuItem;
         private System.Windows.Forms.ToolStripMenuItem HelpMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem AboutMenuItem;
         private System.Windows.Forms.ToolStripMenuItem ApiKeyMenuItem;
         private System.Windows.Forms.DataGridViewTextBoxColumn Id;
         private System.Windows.Forms.DataGridViewTextBoxColumn SongName;
@@ -253,5 +254,6 @@ namespace RagnaCustoms.Views
         private System.Windows.Forms.DataGridViewTextBoxColumn SongAuthor;
         private System.Windows.Forms.DataGridViewTextBoxColumn SongMapper;
         private System.Windows.Forms.DataGridViewButtonColumn SongDownload;
+        private System.Windows.Forms.ToolStripMenuItem logFileToolStripMenuItem;
     }
 }
