adafff800d8f91d0595fd38e9b6da2ff2677072e
OcelusPRO <ocelus1999@hotmail.com>
2021-08-14 21:51:37 +0200

    rework command system

---
+++ src/RagnaCustoms.App/Views/Commandes/NextCommand.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
+using TwitchLib.Client;
+using TwitchLib.Client.Events;
+using TwitchLib.Client.Models;
+
+namespace RagnaCustoms.App.Views.Commandes
+{
+    class NextCommand: ICommandes
+    {
+        List<string> ICommandes.names()
+        {
+            return new List<string>() { "next" };
+        }
+        string ICommandes.help()
+        {
+            return "?";
+        }
+        bool ICommandes.action(
+            JoinedChannel joinedChannel, 
+            TextBox prefixe, 
+            TwitchClient client, 
+            TwitchBotForm me, 
+            OnMessageReceivedArgs e
+        )
+        {
+            var song = me.songRequests.Rows[0].Cells["Song"].Value.ToString();
+            if (song != null)
+            {
+                client.SendMessage(joinedChannel, $"Next song : {song} ");
+            }
+            else
+            {
+                client.SendMessage(joinedChannel, $"End of the queue");
+            }
+            return true;
+        }
+    }
+
+}
---
+++ src/RagnaCustoms.App/Views/Commandes/OpenQueue.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Windows.Input;
+using TwitchLib.Client;
+using TwitchLib.Client.Events;
+using TwitchLib.Client.Models;
+
+namespace RagnaCustoms.App.Views.Commandes
+{
+    class OpenQueue: ICommandes
+    {
+        List<string> ICommandes.names()
+        {
+            return new List<string>() { "open" };
+        }
+        string ICommandes.help()
+        {
+            return "Vous permet d'ouvrir la queue (moderateur uniquement)";
+        }
+        bool ICommandes.action(
+            JoinedChannel joinedChannel, 
+            TextBox prefixe, 
+            TwitchClient client, 
+            TwitchBotForm me, 
+            OnMessageReceivedArgs e
+        )
+        {
+            if (e.ChatMessage.UserType == TwitchLib.Client.Enums.UserType.Viewer)
+            {
+                client.SendMessage(joinedChannel, $"Sorry only moderator can do that");
+            }
+            if (!me.QueueIsOpen)
+            {
+                me.QueueIsOpen = true;
+                client.SendMessage(joinedChannel, $"Queue is now open");
+            }
+            else
+            {
+                client.SendMessage(joinedChannel, $"Queue is already open");
+            }            
+            return true;
+        }
+    }
+
+}
