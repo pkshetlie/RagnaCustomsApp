25fc7906dd275930daec44137da3d58465e7a7af
pkshetlie <pierrick.pobelle@gmail.com>
2022-01-09 14:10:37 +0100

    change text button

--- src/RagnaCustoms.App/Views/TwitchBotForm.Designer.cs
+++ src/RagnaCustoms.App/Views/TwitchBotForm.Designer.cs
@@ -233,7 +233,7 @@ namespace RagnaCustoms.App.Views
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(121, 23);
             this.button1.TabIndex = 36;
-            this.button1.Text = "button1";
+            this.button1.Text = "!rc shift";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
