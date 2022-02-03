ae150848411a50e5a126f75859f0eb9a254030f3
OcelusPRO <ocelus1999@hotmail.com>
2022-02-03 16:45:38 +0100

    fix non working code

--- src/RagnaCustoms.App/Services/FileChangeEvent.cs
+++ src/RagnaCustoms.App/Services/FileChangeEvent.cs
@@ -7,28 +7,23 @@ namespace RagnaCustoms.App.Services
 {
     public class FileChangeEvent
     {
-        private string _path;
-        private Action<object, FileSystemEventArgs, FileChangeEvent> _onChangeEvent;
-        private FileSystemWatcher _watcher;
-        public TwitchBotForm _twitchBotForm = null;
+        private Action<object, FileSystemEventArgs> _onChangeEvent;
         
         public FileChangeEvent(string path, string filter)
         {
-            using var watcher = new FileSystemWatcher(path);
-            watcher.NotifyFilter = NotifyFilters.LastWrite;
-            watcher.Filter = filter;
-            watcher.Changed += OnChange;
-            watcher.Error += OnError;
-            
-            _path = path;
-            _watcher = watcher;
-        }
-        public void SetTwitchBotForm(TwitchBotForm form)
-        {
-            _twitchBotForm = form;
+            new Thread(() =>
+            {
+                using var watcher = new FileSystemWatcher($@"{path}", filter);
+                watcher.NotifyFilter = NotifyFilters.LastWrite;
+                watcher.Changed += new FileSystemEventHandler(OnChange);
+                watcher.Error += new ErrorEventHandler(OnError);
+                watcher.IncludeSubdirectories = true;
+                watcher.EnableRaisingEvents = true;
+                while (true);
+            }).Start();
         }
 
-        public void SetLambda(Action<object, FileSystemEventArgs, FileChangeEvent> onChange)
+        public void SetLambda(Action<object, FileSystemEventArgs> onChange)
         {
             _onChangeEvent = onChange;
         }
@@ -36,8 +31,7 @@ namespace RagnaCustoms.App.Services
         private void OnChange(object sender, FileSystemEventArgs e)
         {
             if (e.ChangeType != WatcherChangeTypes.Changed) return;
-            var me = this;
-            new Thread(() => { _onChangeEvent(sender, e, me); }).Start();
+            new Thread(() => { _onChangeEvent(sender, e); }).Start();
         }
         private static void OnError(object sender, ErrorEventArgs e) => PrintException(e.GetException());
         private static void PrintException(Exception? ex) 
